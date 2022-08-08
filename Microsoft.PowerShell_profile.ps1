1e7de4a6b9d0f96a1215c0d1538028173767354d
Andy Mehalick <andy.mehalick@outlook.com>
2022-08-08 10:40:44 -0400

    update to use base _profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,1 @@
-# https://ohmyposh.dev/docs/installation/windows
-
-oh-my-posh --init --shell pwsh --config .\_.omp.json | Invoke-Expression
-
-Import-Module -Name Terminal-Icons
+. $PSScriptRoot\_profile.ps1
