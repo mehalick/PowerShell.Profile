52396d723917701393ed80b2a915e17bd9041fc3
Andy Mehalick <andy.mehalick@outlook.com>
2022-08-12 10:57:00 -0400

    add mcd cmdlet

--- _profile.ps1
+++ _profile.ps1
@@ -1,3 +1,22 @@
+# https://ohmyposh.dev/docs/windows
+
 oh-my-posh --init --shell pwsh --config .\_.omp.json | Invoke-Expression
 
+# Install-Module -Name Terminal-Icons
+
 Import-Module -Name Terminal-Icons
+
+# Functions
+
+function New-DirectoryAndSetLocation {
+    [alias("mcd")]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        $Path
+    )
+ 
+    New-Item -Path $Path -ItemType Directory
+ 
+    Set-Location -Path $Path
+}
