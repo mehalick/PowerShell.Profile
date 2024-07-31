da0fec05e18600c251e31fb1aa97979a44ab6958
Andy Mehalick <andy.mehalick@outlook.com>
2024-07-31 11:15:37 -0400

    update blocks

--- _profile.ps1
+++ _profile.ps1
@@ -1,13 +1,13 @@
 # https://ohmyposh.dev/docs/windows
 
-oh-my-posh --init --shell pwsh --config .\_.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\Documents\PowerShell\_.omp.json" | Invoke-Expression
+#oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 
 # Install-Module -Name Terminal-Icons
 
 Import-Module -Name Terminal-Icons
 
 # Functions
-
 function New-DirectoryAndSetLocation {
     [alias("mcd")]
     [CmdletBinding()]
